ba164db20bb835dac78fc4c27411098e0fedb698
Prince Kumar Mishra <kmishraprince@gmail.com>
2026-01-14 15:02:51 +0530

    Sidebar Added

---
+++ app/_components/AppSidebar.jsx
@@ -0,0 +1,47 @@
+"use client"
+import { Button } from "@/components/ui/button"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+} from "@/components/ui/sidebar"
+import { Moon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
+import Image from "next/image"
+
+export function AppSidebar() {
+  const {theme, setTheme} = useTheme()
+  return (
+    <Sidebar>
+      <SidebarHeader />
+      <div className="p-3">
+      <div className="p-3 flex justify-between items-center">
+      <div className="flex items-center gap-3">
+        <Image src="/logo.svg" alt="logo" width={60} height={60} className="w-[40px] h-[40px]"/>
+        <h2 className="font-bold text-xl">ThinkStack</h2>
+      </div>
+      <div>
+        {theme=='light' ? <Button variant="ghost" onClick={() => setTheme('dark')}> <Sun /></Button>
+        :
+        <Button variant="ghost" onClick={() => setTheme('light')}> <Moon /></Button>
+        }
+      </div>
+      </div>
+      <Button className={'mt-7 w-full'} size="lg">+ New Chat</Button>
+      </div>
+      <SidebarContent>
+        <SidebarGroup className={'p-3'}>
+          <h2 className="font-bold text-lg">Chat</h2>
+          <p className="text-sm text-gray-400">Sign in to start chatting with multiple AI model</p>
+        </ SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <div className="p-3 mb-10">
+          <Button className={'w-full'} size="lg">Sign In / Sign Up</Button>
+        </div>
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
--- app/provider.jsx
+++ app/provider.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
+import { AppSidebar } from './_components/AppSidebar'
 
 function Provider({
   children,
@@ -13,7 +15,11 @@ function Provider({
         disableTransitionOnChange
         {...props}
     >
-    <div>{children}</div>
+        <SidebarProvider>
+            <AppSidebar />
+            <SidebarTrigger />
+            <div>{children}</div>
+        </SidebarProvider>
     </NextThemesProvider>
   )
 }
