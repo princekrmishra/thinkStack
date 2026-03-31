624739b06f21e7112cd9abfb2ac75d569d2ed502
Prince Kumar Mishra <kmishraprince@gmail.com>
2026-04-01 02:48:28 +0530

    Fix drizzle config env typing

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -10,7 +10,7 @@ export default {
   out: "./drizzle/migrations",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL!,
   },
   verbose: true,
   strict: true,
